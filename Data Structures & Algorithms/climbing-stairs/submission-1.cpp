e94ad9b4b0ce17bdb215f32c62199f13523f7f75
charantejanaik <68796160+charanteja2407@users.noreply.github.com>
2026-04-23 18:26:39 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int dp[46] = {0};
+    int climbStairs(int n) {
+        
+        if(n <= 1) return 1;
+
+        if(dp[n] != 0) return dp[n];
+
+        
+        return  dp[n] = climbStairs(n-2) + climbStairs(n-1);
+    } 
+};
