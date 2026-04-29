55a9ee79d797d35b88aaad171aecfa0821ec0802
charantejanaik <68796160+charanteja2407@users.noreply.github.com>
2026-04-29 21:40:06 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+        vector<vector<int>> res;
+        vector<int> temp;
+        sort(nums.begin(), nums.end());
+        dp(nums, 0, temp, res);
+
+        return vector<vector<int>> (res.begin(), res.end());
+    }
+
+    void dp(vector<int> &nums, int i, vector<int> &temp, vector<vector<int>> &res){
+        if(i == nums.size()){
+            res.push_back(temp);   
+            return;
+        }
+        // take
+        temp.push_back(nums[i]);
+        dp(nums, i+1, temp, res);
+        temp.pop_back();
+        // no take
+        int ni = i+1;
+        while(ni < nums.size() && nums[ni] == nums[i])   ni++;
+        dp(nums, ni, temp, res);
+
+    }
+};
